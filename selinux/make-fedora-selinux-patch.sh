f9af9d17470dda4a00a6ca8b5fd18c2db2d52323
Petr Lautrbach <plautrba@redhat.com>
2016-02-21 11:26:53 +0100

    selinux/make-fedora-selinux-patch.sh
    Initial version of the script used to create <package>-fedora.patch in
    Fedora's spec files.
    
    Usage:
            $ ./make-fedora-selinux-patch.sh <package>
    
    Variables:
            VERSION - release version
                    - VERSION=2.5
            REPO    - a repository to create a patch from
                    - REPO=https://github.com/fedora-selinux/selinux
            BRANCH  - a branch in the repository
                    - BRANCH=master
    
    Usage with changed variables:
            $ VERSION=2.5-rc1 REPO=/home/bachradsusi/devel/github/bachradsusi/selinux.git  ./make-fedora-selinux-patch.sh libsepol

---
+++ selinux/make-fedora-selinux-patch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	cat <<EOF
+Usage:
+	\$ $0 <package>
+
+Variables:
+	VERSION	- release version
+		- VERSION=2.5
+	REPO	- a repository to create a patch from
+		- REPO=https://github.com/fedora-selinux/selinux
+	BRANCH	- a branch in the repository
+		- BRANCH=master
+
+Usage with changed variables:
+	\$ VERSION=2.5-rc1 REPO=/home/bachradsusi/devel/github/bachradsusi/selinux.git  $0 libsepol
+
+EOF
+	exit 1
+fi
+
+PACKAGE=$1
+VERSION=${VERSION:-2.5}
+REPO=${REPO:-https://github.com/fedora-selinux/selinux}
+BRANCH=${BRANCH:-master}
+
+REBASEDIR=`mktemp -d rebase.XXXXXX`
+
+pushd $REBASEDIR
+
+git clone $REPO
+
+pushd selinux
+git checkout $BRANCH
+COMMIT=`git rev-parse --verify HEAD`
+popd
+
+# prepare $PACKAGE-fedora.patch
+tar xfz ../$PACKAGE-$VERSION.tar.gz
+
+pushd $PACKAGE-$VERSION
+git init; git add .; git commit -m "init"
+cp -r ../selinux/$PACKAGE/* .
+git add -A .
+git diff --cached --src-prefix=$PACKAGE-$VERSION/ --dst-prefix=$PACKAGE-$VERSION/ > ../../$PACKAGE-fedora.patch
+popd
+
+popd
+# echo rm -rf $REBASEDIR
+
+echo $PACKAGE-fedora.patch created from $REPO/commit/$COMMIT
